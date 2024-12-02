698506a01298ea646e7a99c2e56159f3c7025f74
Turbobot <turbobot@vercel.com>
2024-12-03 00:35:43 +0530

    feat(create-turbo): apply official-starter transform

--- frontend/src/types/index.ts
+++
@@ -1,6 +0,0 @@
-import { Square, PieceSymbol, Color } from "chess.js";
-
-export type boardType = ({ square: Square; type: PieceSymbol; color: Color; } | null)[][];
-export const INIT_GAME = "init_game"
-export const MOVE = "move"
-export const GAME_OVER = "game_over"
