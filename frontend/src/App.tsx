df2a7b429b0064895ff1bc940b7c439bb2dca007
Nikhil Kumar <nikhilnainwaya17@gmail.com>
2024-12-24 18:11:50 +0530

    Created the Auth page UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./screens/Landing";
 import Game from "./screens/Game";
+import Auth from "./screens/Auth";
 function App() {
     return (
         <div className="bg-gray-900 h-screen ">
@@ -8,6 +9,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Landing/>} />
                     <Route path="/game" element={<Game/>} />
+                    <Route path="/auth" element={<Auth/>} />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ frontend/src/components/Heading.tsx
@@ -0,0 +1,10 @@
+
+const Heading = ({ title }: { title: string }) => {
+    return (
+        <div className="text-green-600 font-extrabold text-5xl mb-10 text-center">
+            {title}
+        </div>
+    )
+}
+
+export default Heading
